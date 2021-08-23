775c35c8c72053cdcd5fa4de6f4c926a0f4a03a2
Astpod <49484285+Astpod@users.noreply.github.com>
2021-08-23 10:33:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ mongoose.connect(cfg.mongoURL, {
   })
 
 const client = new Client();
-client.login();
+client.login(cfg.token);
 
 client.toDate = date => {
     return new Date(date).toLocaleString("tr-TR", { hour12: false, timeZone: "Europe/Istanbul"}).replace(",", "");
